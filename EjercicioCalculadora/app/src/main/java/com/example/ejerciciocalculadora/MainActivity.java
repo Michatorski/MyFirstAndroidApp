6d910fe79d25a983197bda10a11c7cb377bd352c
Michatorski <MichalHomend@protonmail.com>
2021-10-02 15:20:18 +0200

    Making Ejercicio Calculadora

--- EjercicioCalculadora/app/src/main/java/com/example/ejerciciocalculadora/MainActivity.java
+++ EjercicioCalculadora/app/src/main/java/com/example/ejerciciocalculadora/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ejerciciocalculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,80 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void cargarSuma(View v) {
+        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
+        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
+
+        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
+
+        String sNum1 = eNum1.getText().toString();
+        String sNum2 = eNum2.getText().toString();
+
+        int num1 = Integer.parseInt(sNum1);
+        int num2 = Integer.parseInt(sNum2);
+
+        int sum = num1 + num2;
+        tValorRes.setText(Integer.toString(sum));
+    }
+
+    public void cargarResta(View v) {
+        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
+        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
+
+        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
+
+        String sNum1 = eNum1.getText().toString();
+        String sNum2 = eNum2.getText().toString();
+
+        int num1 = Integer.parseInt(sNum1);
+        int num2 = Integer.parseInt(sNum2);
+
+        int sum = num1 - num2;
+        tValorRes.setText(Integer.toString(sum));
+    }
+
+    public void cargarMulti(View v) {
+        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
+        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
+
+        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
+
+        String sNum1 = eNum1.getText().toString();
+        String sNum2 = eNum2.getText().toString();
+
+        int num1 = Integer.parseInt(sNum1);
+        int num2 = Integer.parseInt(sNum2);
+
+        int sum = num1 * num2;
+        tValorRes.setText(Integer.toString(sum));
+    }
+
+    public void cargarDiv(View v) {
+        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
+        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
+
+        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
+
+        String sNum1 = eNum1.getText().toString();
+        String sNum2 = eNum2.getText().toString();
+
+        int num1 = Integer.parseInt(sNum1);
+        int num2 = Integer.parseInt(sNum2);
+
+        int sum = num1 / num2;
+        tValorRes.setText(Integer.toString(sum));
+    }
+
+    public void reset(View v) {
+        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
+        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
+
+        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
+
+        eNum1.setText(" ");
+        eNum2.setText(" ");
+        tValorRes.setText(" ");
     }
 }
