ab22d46fefde08b40230b4d0e9692b06070a054e
Michatorski <MichalHomend@protonmail.com>
2021-10-06 18:12:43 +0200

    Adding IFs

--- EjercicioCalculadora/app/src/main/java/com/example/ejerciciocalculadora/MainActivity.java
+++ EjercicioCalculadora/app/src/main/java/com/example/ejerciciocalculadora/MainActivity.java
@@ -9,82 +9,85 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText eNum1, eNum2;
+    private TextView tValorRes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eNum1 = (EditText) findViewById(R.id.etNum1);
+        eNum2 = (EditText) findViewById(R.id.etNum2);
+
+        tValorRes = (TextView) findViewById(R.id.tValorRes);
     }
 
     public void cargarSuma(View v) {
-        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
-        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
 
-        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
+        if (eNum1.getText().toString().isEmpty() || eNum2.getText().toString().isEmpty())
+        { tValorRes.setText("Introduce los números"); }
+        else {
+            String sNum1 = eNum1.getText().toString();
+            String sNum2 = eNum2.getText().toString();
 
-        String sNum1 = eNum1.getText().toString();
-        String sNum2 = eNum2.getText().toString();
+            int num1 = Integer.parseInt(sNum1);
+            int num2 = Integer.parseInt(sNum2);
 
-        int num1 = Integer.parseInt(sNum1);
-        int num2 = Integer.parseInt(sNum2);
-
-        int res = num1 + num2;
-        tValorRes.setText(Integer.toString(res));
+            int res = num1 + num2;
+            tValorRes.setText(Integer.toString(res));
+        }
     }
 
     public void cargarResta(View v) {
-        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
-        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
 
-        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
+        if (eNum1.getText().toString().isEmpty() || eNum2.getText().toString().isEmpty())
+        { tValorRes.setText("Introduce los números"); }
+        else {
+            String sNum1 = eNum1.getText().toString();
+            String sNum2 = eNum2.getText().toString();
 
-        String sNum1 = eNum1.getText().toString();
-        String sNum2 = eNum2.getText().toString();
+            int num1 = Integer.parseInt(sNum1);
+            int num2 = Integer.parseInt(sNum2);
 
-        int num1 = Integer.parseInt(sNum1);
-        int num2 = Integer.parseInt(sNum2);
-
-        int res = num1 - num2;
-        tValorRes.setText(Integer.toString(res));
+            int res = num1 - num2;
+            tValorRes.setText(Integer.toString(res));
+        }
     }
 
     public void cargarMulti(View v) {
-        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
-        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
 
-        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
+        if (eNum1.getText().toString().isEmpty() || eNum2.getText().toString().isEmpty())
+        { tValorRes.setText("Introduce los números"); }
+        else {
+            String sNum1 = eNum1.getText().toString();
+            String sNum2 = eNum2.getText().toString();
 
-        String sNum1 = eNum1.getText().toString();
-        String sNum2 = eNum2.getText().toString();
+            int num1 = Integer.parseInt(sNum1);
+            int num2 = Integer.parseInt(sNum2);
 
-        int num1 = Integer.parseInt(sNum1);
-        int num2 = Integer.parseInt(sNum2);
-
-        int res = num1 * num2;
-        tValorRes.setText(Integer.toString(res));
+            int res = num1 * num2;
+            tValorRes.setText(Integer.toString(res));
+        }
     }
 
     public void cargarDiv(View v) {
-        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
-        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
+        if (eNum1.getText().toString().isEmpty() || eNum2.getText().toString().isEmpty())
+        { tValorRes.setText("Introduce los números"); }
+        else {
+            String sNum1 = eNum1.getText().toString();
+            String sNum2 = eNum2.getText().toString();
 
-        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
-
-        String sNum1 = eNum1.getText().toString();
-        String sNum2 = eNum2.getText().toString();
-
-        int num1 = Integer.parseInt(sNum1);
-        int num2 = Integer.parseInt(sNum2);
+            int num1 = Integer.parseInt(sNum1);
+            int num2 = Integer.parseInt(sNum2);
 
             int res = num1 / num2;
             tValorRes.setText(Integer.toString(res));
+        }
 
     }
 
     public void reset(View v) {
-        EditText eNum1 = (EditText) findViewById(R.id.etNum1);
-        EditText eNum2 = (EditText) findViewById(R.id.etNum2);
-
-        TextView tValorRes = (TextView) findViewById(R.id.tValorRes);
 
         eNum1.setText(" ");
         eNum2.setText(" ");
