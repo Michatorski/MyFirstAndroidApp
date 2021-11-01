c80ffc2a68f8fa47960f3e5195ac0c34a81acb0f
Michatorski <MichalHomend@protonmail.com>
2021-11-01 10:57:12 +0100

    Completando cosas

--- ActivitiesExcer1/app/src/main/java/com/example/activitiesexcer1/MainActivity.java
+++ ActivitiesExcer1/app/src/main/java/com/example/activitiesexcer1/MainActivity.java
@@ -2,13 +2,71 @@ package com.example.activitiesexcer1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    protected static final String MAINACTIVITY_NAME = "com.example.activitiesexer1.name";
+    protected static final String MAINACTIVITY_SURNAME = "com.example.activitiesexer1.surname";
+    protected static final String MAINACTIVITY_AGE = "com.example.activitiesexer1.age";
+    protected static final String MAINACTIVITY_LICENCE = "com.example.activitiesexer1.licence";
+
+    EditText name, surname, age;
+    CheckBox checkCarne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText) findViewById(R.id.et_name);
+        surname = (EditText) findViewById(R.id.et_surname);
+        age = (EditText) findViewById(R.id.et_age);
+        checkCarne = (CheckBox) findViewById(R.id.cb_carne);
+    }
+
+    public void chargeInfo(View v) {
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        String nameMessage = name.getText().toString();
+
+        String surnameMessage = surname.getText().toString();
+
+        String ageMessage = age.getText().toString();
+
+        String licenceMessage = "Tiene carne de conducir";
+        String noLicenceMessage = "No tiene carne de conducir";
+
+
+        intent.putExtra(MAINACTIVITY_NAME, nameMessage);
+
+
+        intent.putExtra(MAINACTIVITY_SURNAME, surnameMessage);
+
+
+        intent.putExtra(MAINACTIVITY_AGE, ageMessage);
+
+
+        if (checkCarne.isChecked()) {
+            intent.putExtra(MAINACTIVITY_LICENCE, licenceMessage);
+        } else {
+            intent.putExtra(MAINACTIVITY_LICENCE, noLicenceMessage);
+        }
+
+        if (nameMessage.isEmpty()) {
+            Toast.makeText(this, "Se necesta rellenar el NOMBRE", Toast.LENGTH_SHORT).show();
+        } else if (ageMessage.isEmpty()) {
+            Toast.makeText(this, "Se necesta rellenar la EDAD", Toast.LENGTH_SHORT).show();
+        } else if (surnameMessage.isEmpty()) {
+            Toast.makeText(this, "Se necesta rellenar los APELLIDOS", Toast.LENGTH_SHORT).show();
+        } else {
+            startActivity(intent);
+        }
+
+
     }
 }
--- ActivitiesExcer1/app/src/main/java/com/example/activitiesexcer1/MainActivity2.java
+++ ActivitiesExcer1/app/src/main/java/com/example/activitiesexcer1/MainActivity2.java
@@ -5,20 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-    protected  static  final String MAINACTIVITY_NAME = "com.example.activitiesexer1.name";
-    protected  static  final String MAINACTIVITY_SURNAME = "com.example.activitiesexer1.surname";
-    protected  static  final String MAINACTIVITY_AGE = "com.example.activitiesexer1.age";
+    TextView name, surname, age, licence;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String nameMessage = intent.getStringExtra(MainActivity.MAINACTIVITY_NAME);
+        String surnameMessage = intent.getStringExtra(MainActivity.MAINACTIVITY_SURNAME);
+        String ageMessage = intent.getStringExtra(MainActivity.MAINACTIVITY_AGE);
+
+        String licenceMessage = intent.getStringExtra(MainActivity.MAINACTIVITY_LICENCE);
+
+        name = (TextView) findViewById(R.id.tv_name);
+        name.setText(nameMessage);
+        surname = (TextView) findViewById(R.id.tv_surname);
+        surname.setText(surnameMessage);
+        age = (TextView) findViewById(R.id.tv_age);
+        age.setText(ageMessage);
+        licence = (TextView) findViewById(R.id.tv_carne);
+        licence.setText(licenceMessage);
     }
 
-    public void chargeInfo(View v){
-        Intent pantallaSecundaia = new Intent(this, MainActivity2.class);
-
-    }
 }
