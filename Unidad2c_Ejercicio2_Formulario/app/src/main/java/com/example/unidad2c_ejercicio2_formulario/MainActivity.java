a7b167e723782b0b3674b5c0b6ffd5f559fef7f3
MicHo <MichalHomend@protonmail.com>
2021-11-11 09:39:27 +0100

    First commit for Formulario ejercicio2

---
+++ Unidad2c_Ejercicio2_Formulario/app/src/main/java/com/example/unidad2c_ejercicio2_formulario/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.unidad2c_ejercicio2_formulario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
