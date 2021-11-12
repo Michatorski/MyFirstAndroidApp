2cca711373360393c509d1dc9f64f66fc4520c5f
MicHo <MichalHomend@protonmail.com>
2021-11-12 13:03:47 +0100

    haciendo el codigo

--- Unidad2c_Ejercicio2_Formulario/app/src/main/java/com/example/unidad2c_ejercicio2_formulario/MainActivity.java
+++ Unidad2c_Ejercicio2_Formulario/app/src/main/java/com/example/unidad2c_ejercicio2_formulario/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected final static String  EMAIL = "com.example.email";
     protected final static String  POSITION = "com.example.position";
 
-    Spinner sContacts, sSports;
+    Spinner spinContacts, spinSports;
 
     ArrayAdapter<String> adapter1 , adapter2;
 
@@ -30,17 +32,29 @@ public class MainActivity extends AppCompatActivity {
         String[] contactType = {"Movil", "Correo electronico"};
         String[] sport = {"Futbol", "Baloncesto"};
 
-        sContacts = (Spinner) findViewById(R.id.sContact);
-        sSports = (Spinner) findViewById(R.id.sSports);
+        spinContacts = (Spinner) findViewById(R.id.sContact);
+        spinSports = (Spinner) findViewById(R.id.sSports);
 
         adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, contactType);
-        sContacts.setAdapter(adapter1);
+        spinContacts.setAdapter(adapter1);
 
         adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sport);
-        sSports.setAdapter(adapter2);
+        spinSports.setAdapter(adapter2);
 
 
         Intent intent = new Intent(this, MainActivity2.class);
+
+        spinContacts.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
     }
