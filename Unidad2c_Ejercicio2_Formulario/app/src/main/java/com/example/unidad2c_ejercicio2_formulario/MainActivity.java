14c03a705066414022491298a976d0a220592e1e
Michatorski <MichalHomend@protonmail.com>
2021-11-13 03:33:32 +0100

    Cambiando cosas, error al arrancar, posiblemente por los GONE

--- Unidad2c_Ejercicio2_Formulario/app/src/main/java/com/example/unidad2c_ejercicio2_formulario/MainActivity.java
+++ Unidad2c_Ejercicio2_Formulario/app/src/main/java/com/example/unidad2c_ejercicio2_formulario/MainActivity.java
@@ -7,30 +7,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    protected final static String  NAME = "com.example.name";
-    protected final static String  SURNAME = "com.example.surname";
-    protected final static String  SPORT = "com.example.sport";
-    protected final static String  MOBILE = "com.example.mobile";
-    protected final static String  EMAIL = "com.example.email";
-    protected final static String  POSITION = "com.example.position";
+    protected final static String NAME = "com.example.name";
+    protected final static String SURNAME = "com.example.surname";
+    protected final static String SPORT = "com.example.sport";
+    protected final static String MOBILE = "com.example.mobile";
+    protected final static String EMAIL = "com.example.email";
+    protected final static String POSITION = "com.example.position";
+
 
     Spinner spinContacts, spinSports;
 
-    ArrayAdapter<String> adapter1 , adapter2;
+    ArrayAdapter<String> adapter1, adapter2;
+
+    TextView tvEmail, tvMobile, mandatoryMobile, mandatoryEmail;
+    EditText etEmail, etMobile, etName, etSurname;
+    RadioButton position1, position2, position3, position4;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+
+
     }
 
-    public void initialize(){
+    public void initialize() {
 
-        String[] contactType = {"Movil", "Correo electronico"};
-        String[] sport = {"Futbol", "Baloncesto"};
+        etName = (EditText) findViewById(R.id.etName);
+        etSurname = (EditText) findViewById(R.id.etSurname);
+        etMobile = (EditText) findViewById(R.id.etMovile);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+
+        String[] contactType = {"Selecciona...", "Movil", "E-mail"};
+        String[] sport = {"Selecciona...", "Fútbol", "Baloncesto"};
 
         spinContacts = (Spinner) findViewById(R.id.sContact);
         spinSports = (Spinner) findViewById(R.id.sSports);
@@ -41,13 +62,51 @@ public class MainActivity extends AppCompatActivity {
         adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sport);
         spinSports.setAdapter(adapter2);
 
+        position1 = (RadioButton) findViewById(R.id.rbPosition1);
+        position2 = (RadioButton) findViewById(R.id.rbPosition2);
+        position3 = (RadioButton) findViewById(R.id.rbPosition3);
+        position4 = (RadioButton) findViewById(R.id.rbPosition4);
 
-        Intent intent = new Intent(this, MainActivity2.class);
 
+        tvEmail.setVisibility(View.GONE);
+        etEmail.setVisibility(View.GONE);
+        mandatoryEmail.setVisibility(View.GONE);
+
+        tvMobile.setVisibility(View.GONE);
+        etMobile.setVisibility(View.GONE);
+        mandatoryMobile.setVisibility(View.GONE);
+
+        adapterContact();
+        adapterSport();
+
+    }
+
+    public void adapterContact() {
         spinContacts.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                String selected = parent.getSelectedItem().toString();
+                String selectedByPosition = parent.getItemAtPosition(position).toString();
+                String res = "Selected: " + selected + " SelectedByPosition: " + selectedByPosition +
+                        " position: " + position;
+                Toast.makeText(parent.getContext(), res, Toast.LENGTH_SHORT).show();
+                if (selected.equals("Movil")) {
+                    tvEmail.setVisibility(View.GONE);
+                    etEmail.setVisibility(View.GONE);
+                    mandatoryEmail.setVisibility(View.GONE);
 
+                    tvMobile.setVisibility(View.VISIBLE);
+                    etMobile.setVisibility(View.VISIBLE);
+                    mandatoryMobile.setVisibility(View.VISIBLE);
+                } else if (selected.equals("E-mail"))  {
+                    tvEmail.setVisibility(View.VISIBLE);
+                    etEmail.setVisibility(View.VISIBLE);
+                    mandatoryEmail.setVisibility(View.VISIBLE);
+
+                    tvMobile.setVisibility(View.GONE);
+                    etMobile.setVisibility(View.GONE);
+                    mandatoryMobile.setVisibility(View.GONE);
+                }
             }
 
             @Override
@@ -55,7 +114,58 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    public void adapterSport(){
+        spinSports.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                String selected = parent.getSelectedItem().toString();
+                String selectedByPosition = parent.getItemAtPosition(position).toString();
+                String res = "Selected: " + selected + " SelectedByPosition: " + selectedByPosition +
+                        " position: " + position;
+                Toast.makeText(parent.getContext(), res, Toast.LENGTH_SHORT).show();
+                if (selected.equals("Fútbol")){
+                    position1.setText("Portero");
+                    position2.setText("Defensa");
+                    position3.setText("Medio");
+                    position4.setText("Delantero");
+                } else if (selected.equals("Baloncesto")){
+                    position1.setText("Base");
+                    position2.setText("Escolta");
+                    position3.setText("Alero");
+                    position4.setText("Ala-pivot");
+                }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    public void sendInfo(View v){
+
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        intent.putExtra(NAME, etName.toString());
+        intent.putExtra(SURNAME, etSurname.toString());
+        intent.putExtra(MOBILE, etMobile.toString());
+        intent.putExtra(EMAIL, etEmail.toString());
+        intent.putExtra(SPORT, spinSports.getSelectedItem().toString());
+
+        if (position1.isChecked()){
+            intent.putExtra(POSITION, position1.toString());
+        }
+        if (position2.isChecked()){
+            intent.putExtra(POSITION, position2.toString());
+        }
+        if (position3.isChecked()){
+            intent.putExtra(POSITION, position3.toString());
+        }
+        if (position4.isChecked()){
+            intent.putExtra(POSITION, position4.toString());
+        }
     }
 }
--- Unidad2c_Ejercicio2_Formulario/app/src/main/java/com/example/unidad2c_ejercicio2_formulario/MainActivity2.java
+++ Unidad2c_Ejercicio2_Formulario/app/src/main/java/com/example/unidad2c_ejercicio2_formulario/MainActivity2.java
@@ -2,13 +2,53 @@ package com.example.unidad2c_ejercicio2_formulario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    TextView name, surname, contact, sport, position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        initialize();
+    }
+
+    public void initialize() {
+        name = (TextView) findViewById(R.id.tvResName_main2);
+        surname = (TextView) findViewById(R.id.tvResSurname_main2);
+        contact = (TextView) findViewById(R.id.tvResContacto_Main2);
+        sport = (TextView) findViewById(R.id.tvResSports_Main2);
+        position = (TextView) findViewById(R.id.tvResPosition_Main2);
+
+        infoRecived();
+
+    }
+
+    public  void infoRecived(){
+        Intent intent = getIntent();
+        String nameResString = intent.getStringExtra(MainActivity.NAME);
+        String surnameResString = intent.getStringExtra(MainActivity.SURNAME);
+        String mobileResString = intent.getStringExtra(MainActivity.MOBILE);
+        String emailResString = intent.getStringExtra(MainActivity.EMAIL);
+        String sportResString = intent.getStringExtra(MainActivity.SPORT);
+        String positionResString = intent.getStringExtra(MainActivity.POSITION);
+
+        name.setText(nameResString);
+        surname.setText(surnameResString);
+
+        if (!mobileResString.equals("")){
+            contact.setText(mobileResString);
+        }
+        if (!emailResString.equals("")){
+            contact.setText(emailResString);
+        }
+
+        sport.setText(sportResString);
+        position.setText(positionResString);
+
     }
 }
