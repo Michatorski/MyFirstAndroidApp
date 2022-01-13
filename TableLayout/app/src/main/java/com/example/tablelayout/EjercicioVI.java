6508fd8b28a48613a07480f62634754248241dc6
MicHo <MichalHomend@protonmail.com>
2022-01-13 10:32:14 +0100

    TableLayout FirstCommit

---
+++ TableLayout/app/src/main/java/com/example/tablelayout/EjercicioVI.java
@@ -0,0 +1,14 @@
+package com.example.tablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EjercicioVI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_vi);
+    }
+}
