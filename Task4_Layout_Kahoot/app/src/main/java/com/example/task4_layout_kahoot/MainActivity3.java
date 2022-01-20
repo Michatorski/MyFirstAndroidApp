69e8e1fcb921cb12baefa922989f264a74fe5ead
MicHo <MichalHomend@protonmail.com>
2022-01-20 14:11:32 +0100

    Haciendo Ejerc sergio

--- Task4_Layout_Kahoot/app/src/main/java/com/example/task4_layout_kahoot/MainActivity3.java
+++ Task4_Layout_Kahoot/app/src/main/java/com/example/task4_layout_kahoot/MainActivity3.java
@@ -3,15 +3,12 @@ package com.example.task4_layout_kahoot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-    EditText nickname;
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
     }
 }
